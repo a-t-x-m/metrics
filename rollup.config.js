2e137e6b358175e7b0ab936c7c057b906392f347
idleberg <git@idleberg.com>
2020-07-19 13:27:10 +0200

    add @rollup/plugin-node-resolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,12 @@
 import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
 const plugins = [
   commonjs(),
+  nodeResolve({
+    preferBuiltins: true
+  }),
   typescript({
     allowSyntheticDefaultImports: true
   })
